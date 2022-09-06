25e8d46d3efbdb00c12d35635a1b4ebcdda91917
Hoang Dung <dungnguyen322003@gmail.com>
2022-09-06 19:19:32 +0800

    add hashing function

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash('passw0rd!', saltRounds, (err, hash) => {
+  /*Store hash in your db*/
+  console.log(hash);
+  bcrypt.compare('passw0rd!', hash, (err, res) => {
+    /*res == true or false*/
+    console.log(res);
+  });
+});
 //END_ASYNC
 
 //START_SYNC
