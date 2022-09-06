c264ab4041a1427860cdbeab346d5cae3953db92
Hoang Dung <dungnguyen322003@gmail.com>
2022-09-06 19:25:05 +0800

    change in hashing function

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash('passw0rd!', saltRounds, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   /*Store hash in your db*/
   console.log(hash);
-  bcrypt.compare('passw0rd!', hash, (err, res) => {
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
     /*res == true or false*/
     console.log(res);
   });
