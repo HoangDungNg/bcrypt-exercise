41f5a263dd8b2a5f6194e1d41274a49b77544fb9
Hoang Dung <dungnguyen322003@gmail.com>
2022-09-06 19:39:21 +0800

    add bcrypt hashSync and compareSync

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //END_ASYNC
 
 //START_SYNC
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
 
 app.listen(process.env.PORT || 3000, () => {});
